d7006ba81e0fbfdb627c23c970ea6fbc9d8e1c7c
Nat10k <156871636+NoArt278@users.noreply.github.com>
2024-11-29 23:00:10 +0700

    feat : maze finish sound

--- scripts/maze_finish.gd
+++ scripts/maze_finish.gd
@@ -1,6 +1,7 @@
 extends Interactable
 
 var level_manager: Node3D
+@onready var audio_stream_player_3d: AudioStreamPlayer3D = $AudioStreamPlayer3D
 
 func _ready() -> void:
 	if get_tree().current_scene.name == "LevelManager" :
@@ -16,5 +17,11 @@ func _on_body_entered(body: Node3D) -> void:
 		_interact()
 		level_manager.add_advance_level()
 		GameManager.maze_finish_count += 1
+		if audio_stream_player_3d.playing : 
+			audio_stream_player_3d.stop()
+		audio_stream_player_3d.play()
 	elif body.name.contains("Player") :
+		if audio_stream_player_3d.playing : 
+			audio_stream_player_3d.stop()
+		audio_stream_player_3d.play()
 		get_tree().change_scene_to_file("res://scenes/finish.tscn")
